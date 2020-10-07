720c4aa5df5962054a9b64dcb7e70b3f025222cc
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-10-06 21:23:13 -0300

    Agrego Clases
    - Clase Driver
    - Clase Person

---
+++ src/Library/Driver.cs
@@ -0,0 +1,14 @@
+namespace Library
+{
+    public class Driver : Person
+    {
+        public int DriverCalification { get; set; }
+        public string Biography { get; set; }
+        public string Vehicle { get; set; }
+        public Driver (string name, string surname, int ci, string image, string vehicle)
+        : base(name,surname,ci,image)
+        {
+            this.Vehicle = vehicle;
+        }
+    }
+}
